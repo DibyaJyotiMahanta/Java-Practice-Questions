7d4ba2f76552ad8f0b3821ca2d6a0eedcea256e3
DibyaJyotiMahanta <mahantasoham@gmail.com>
2024-07-03 22:00:22 +0530

    wednesday's commit

--- Array_Error.java
+++ Array_Error.java
@@ -1,6 +1,5 @@
 
 class Max_Error extends Exception {
-
     public Max_Error(String str) {
         super(str);
     }
@@ -8,18 +7,13 @@ class Max_Error extends Exception {
 
 public class Array_Error {
 
-    public static void main(String[] args) throws Max_Error {
-        try {
-            int arr[] = {1, 2, 3, 4, 5};
+    public static void main(String[] args)throws Max_Error {
+        int arr[] = {1, 2, 3, 4, 5};
             for (int i = 0; i < 10; i++) {
                 System.out.println(arr[i]);
                 if (i == 4){
                     throw new Max_Error("Max retires reached!");
                     }
             }
-            
-        } catch (Max_Error e) {
-            System.out.println(e);
-        }
     }
 }
--- test.java
+++ test.java
@@ -1,29 +1,8 @@
-class MyThread extends Thread{
-    @Override
-    public void run(){
-        int i = 0;
-        while(i<20000){
-            System.out.println("I Love Jeevitha");
-            i++;
-        }
-    }
-}
-
-class MyThread2 extends Thread{
-    @Override
-    public void run(){
-        int i = 0;
-        while(i<20000){
-            System.out.println("I Love Sharmila");
-            i++;
-        }
-    }
-}
 public class test{
-    public static void main(String args[]){
-        MyThread t1 = new MyThread();
-        MyThread2 t2 = new MyThread2();
-        t1.start();
-        t2.start();
+    public static void main(String[] args) {
+        try {
+            
+        } finally {
+        }
     }
 }
