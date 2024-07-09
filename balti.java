2f11907d22d862abb0173ee997d829f54bb90df0
DibyaJyotiMahanta <mahantasoham@gmail.com>
2024-07-09 23:24:35 +0530

    todya's commit

--- balti.java
+++ balti.java
@@ -11,7 +11,7 @@ public class balti {
                 num++;
             }
             for (int j = 1; j <= 2 * n - 2 * i; j++) {
-                System.out.print(" ");
+                System.out.print("");
             }
             num--;
             for (int j = 1; j <= i; j++) {
